8af980bda945a1e5c7b6739efe4e7d41efdaf369
babar007 <babarbaig@babars-MacBook-Pro.local>
2016-12-12 12:00:42 -0800

    No idea

--- projectsulekha/projectsulekha/sulekhaapp/urls.py
+++ projectsulekha/projectsulekha/sulekhaapp/urls.py
@@ -2,8 +2,32 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     url(r'^home/$', views.home),
     url(r'^login/$', views.login),
     url(r'^post/$', views.post),  
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+    url(r'^signup/$', views.signup),
+    url(r'^home$/', views.home),
+    url(r'^login$/', views.login),
+<<<<<<< Updated upstream
+    url(r'^register$/', views.register)  
+>>>>>>> Stashed changes
     
+<<<<<<< Updated upstream
+=======
+>>>>>>> babar
+=======
+    
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 ]
